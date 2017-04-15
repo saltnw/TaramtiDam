f9241eff85d473adca378ff1f262392429a6ab9c
tomer <tomer5404@gmail.com>
2017-04-15 20:55:23 +0300

    base profile created

--- app/src/main/java/com/taramtidam/taramtidam/MainActivity.java
+++ app/src/main/java/com/taramtidam/taramtidam/MainActivity.java
@@ -2,13 +2,44 @@ package com.taramtidam.taramtidam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText userEmail;
+    TextView lastDonateDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test comment by asaf
+
+
+        lastDonateDate = (TextView) findViewById(R.id.lastdonate);
+        userName       = (EditText) findViewById(R.id.UserName);
+        userEmail       = (EditText) findViewById(R.id.UserEmail);
+        Button saveButton = (Button) findViewById(R.id.Savebutton);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                String name  = userName.getText().toString();
+                String email = userEmail.getText().toString();
+            }
+        });
+
+        Button updateDateButton = (Button) findViewById(R.id.donatedToday);
+        updateDateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy ");
+                Date date = new Date();
+                lastDonateDate.setText(dateFormat.format(date));
+            }
+        });
     }
 }
