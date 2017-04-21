f430d6b43c73c6d7447755f8f5e634b4d9ce89ee
saltnw <shoshan.z@gmail.com>
2017-04-21 17:03:09 +0300

    filling the db works, but without the geofire object

--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -2,9 +2,7 @@ var functions = require('firebase-functions');
 var getMdaPage = require('./getMdaPage.js')
 var processMDAPage = require('./processMDAPage.js');
 
-exports.getMdaMobiles =
+exports.getMdaMobilesDB =
   functions.pubsub.topic('daily-tick').onPublish((event) => {
-      getMdaPage().then(processMDAPage);
-    }
-
-  })
+      return getMdaPage().then(function(page) { return processMDAPage(page); }).then(function() { console.log("Done Executing!")});
+    })
--- cloudFunctions/functions/processMDAPage.js
+++ cloudFunctions/functions/processMDAPage.js
@@ -5,79 +5,96 @@ provider: 'google',
 };
 var geocoder = NodeGeocoder(options);
 
+//for the databse push
+var functions = require('firebase-functions');
+var admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+var db = admin.database();
+var ref = db.ref();
+ref.child("MDA").remove();
+var nodeRef = ref.child("MDA");
 
 function onDonationPageReceived(page)
 {
-  //for the databse push
-  var functions = require('firebase-functions');
-  var admin = require("firebase-admin");
-  admin.initializeApp(functions.config().firebase);
-  var db = admin.database();
-  var ref = db.ref();
-  var nodeRef = ref.child("MDA");
-
-  var cheerio = require('cheerio');
- $ = cheerio.load(page)
- var data = new Array();
- $('tr').each(function(i, tr){
-   var children = $(this).children();
-   var dateItem = children.eq(0);
-   var cityItem = children.eq(1);
-   var addressItem = children.eq(3);
-   var descriptionItem = children.eq(2);
-   //var secondAddressItem = children.eq(4).children().eq(0)[0];
-   //console.log("AAAAAAAAAAAAAAAAAAAAAAa");
-   //console.log(secondAddressItem)
-   var startTimeItem = children.eq(5);
-   var endTimeItem = children.eq(6);
-   if (i==0 || !dateItem.text().trim()) return;
-   var row = {
-        "date": dateItem.text(),
-       "city": cityItem.text(),
-        "address": addressItem.text(),
-       "description": descriptionItem.text(),
-       //"second address": secondAddressItem.title,
-       "start time": startTimeItem.text(),
-       "end time": endTimeItem.text(),
-    };
-    //data.push(row);
-    getGeoLocationAndPush(row, nodeRef, i);
-});
-
-}
-
-function getGeoLocationAndPush(row, dbRef, timeout)
-{
-  var fullAdress = row["address"] + " " + row["city"];
-  if (!fullAdress.trim()) return;
-  setTimeout(function() {
-    geocoder.geocode(fullAdress, function(err, res) {
-      if (res)
-      {
-        row["latitude"] = res[0].latitude;
-        row["longitude"] = res[0].longitude;
-        var newMobileRef =  dbRef.push(row);
-        //var geoFire = new GeoFire(newMobileRef)
-        // Create a GeoFire index
-      }
-      if (err)
-      {
-        console.log(fullAdress);
-        console.log(err);
-      }
+  return new Promise((resolve, reject) => {
+    var promiseArray = [];
+    var cheerio = require('cheerio');
+   $ = cheerio.load(page)
+   var data = new Array();
+   $('tr').each(function(i, tr){
+     var children = $(this).children();
+     var dateItem = children.eq(0);
+     var cityItem = children.eq(1);
+     var addressItem = children.eq(3);
+     var descriptionItem = children.eq(2);
+     //var secondAddressItem = children.eq(4).children().eq(0)[0];
+     //console.log("AAAAAAAAAAAAAAAAAAAAAAa");
+     //console.log(secondAddressItem)
+     var startTimeItem = children.eq(5);
+     var endTimeItem = children.eq(6);
+     if (i==0 || !dateItem.text().trim()) return;
+     var row = {
+          "date": dateItem.text(),
+         "city": cityItem.text(),
+          "address": addressItem.text(),
+         "description": descriptionItem.text(),
+         //"second address": secondAddressItem.title,
+         "start time": startTimeItem.text(),
+         "end time": endTimeItem.text(),
+      };
+      //data.push(row);
+      var currPromise = getGeoLocationAndPush(row, i);
+      promiseArray.push(currPromise);
     });
-  }, (timeout+1)*1000)
+    return Promise.all(promiseArray);
+  } )
 }
-module.exports = onDonationPageReceived;
-//todo:
-//1. go over the array of "rows" with the async library
-//2. for each object - call the geo decoder function and save it to the database
-//3. save the key from each push and and use
-/*
-geoFire.set("some_key", [37.79, -122.41]).then(function() {
-  console.log("Provided key has been added to GeoFire");
-}, function(error) {
-  console.log("Error: " + error);
-});
 
-*/
+function getGeoLocationAndPush(row, timeout)
+{
+  return new Promise((resolve, reject) => {
+    var address = row["address"];
+    if (address.trim().length == 0)
+    {
+      address = row["description"];
+    }
+    var fullAddress = address + " " + row["city"];
+    fullAddress = fullAddress.trim();
+    if (fullAddress.length == 0)
+      {
+        resolve("done");
+        return;
+      }
+    setTimeout(function() {
+      geocoder.geocode(fullAddress, function(err, res) {
+        if (res)
+        {
+          if (res.length == 0)
+          {
+              resolve("done");
+              return;
+          }
+          row["latitude"] = res[0].latitude;
+          row["longitude"] = res[0].longitude;
+          var newMobileRef =  nodeRef.push(row)
+          resolve("done");
+          var geoFire = new GeoFire(newMobileRef)
+          //geoFire.set("geoLoc")                              // Create a GeoFire index
+        }
+        else if (err)
+        {
+          console.log(fullAddress);
+          console.log(err);
+          reject("rejected");
+        }
+        else
+          resolve("done");
+      });
+    }, (timeout+1)*500)
+  });
+}
+
+//for testing:
+//var getMdaPage = require("./getMdaPage.js");
+//getMdaPage().then(onDonationPageReceived).then(() => console.log("done!"));
+module.exports = onDonationPageReceived;
