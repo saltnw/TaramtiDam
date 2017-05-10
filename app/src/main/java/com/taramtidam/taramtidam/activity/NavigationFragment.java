608ee72ca3fe86d92a76e454b7d2bff7b7d0dbc7
saltnw <shoshan.z@gmail.com>
2017-05-10 15:17:33 +0300

    added navigation to the mobile from the notification

--- app/src/main/java/com/taramtidam/taramtidam/activity/NavigationFragment.java
+++ app/src/main/java/com/taramtidam/taramtidam/activity/NavigationFragment.java
@@ -2,6 +2,8 @@ package com.taramtidam.taramtidam.activity;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -29,6 +31,19 @@ public class NavigationFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
 
+//todo: use this to implement "donate now" - just replace the address with the relevant one
+
+//        // Create a Uri from an intent string. Use the result to create an Intent.
+//        Uri intentUri = Uri.parse("google.navigation:q=בזל+22,+תל+אביב&mode=w");
+//
+//        // Create an Intent from intentUri. Set the action to ACTION_VIEW
+//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, intentUri);
+//
+//        // Make the Intent explicit by setting the Google Maps package
+//        mapIntent.setPackage("com.google.android.apps.maps");
+//
+//        // Attempt to start an activity that can handle the Intent
+//        startActivity(mapIntent);
 
         // Inflate the layout for this fragment
         return rootView;
