9214fcc873c3b81bafd7fa56f8712451449ebb9f
Asaf <asafraviv@gmail.com>
2017-05-10 12:40:23 +0300

    Adding Drawer , upper Toolbar and Fragments + images + fix to MDA database (TODAY,TOMARROW). Adding 3 more empty fragments ready for work

---
+++ app/src/main/java/com/taramtidam/taramtidam/activity/JustDonatedFragment.java
@@ -0,0 +1,47 @@
+package com.taramtidam.taramtidam.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.taramtidam.taramtidam.R;
+
+
+
+public class JustDonatedFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_justdonated, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
+
